98559057a53d124b79882f1f57a2025361f71a0a
Tyler Long <tyler4long@gmail.com>
2017-12-19 17:57:06 +0800

    Write some skeleton code

--- lib/ringcentral.rb
+++ lib/ringcentral.rb
@@ -6,4 +6,51 @@ class RingCentral
   def self.PRODUCTION_SERVER
     'https://platform.ringcentral.com'
   end
+
+  attr_reader :app_key, :app_secret, :server
+  attr_accessor :auto_refresh
+
+  def initialize(app_key, app_secret, server)
+    @app_key = app_key
+    @app_secret = app_secret
+    @server = server
+    @auto_refresh = true
+  end
+
+  def token
+    @token
+  end
+
+  def token=(value)
+    @token = value
+    if @timer != nil
+      # todo: cancel the timer
+      @timer = nil
+    end
+    if @auto_refresh && value != nil
+      # todo: create the timer
+    end
+  end
+
+  def authorize(username = nil, extension = nil, password = nil, auth_code = nil, redirect_uri = nil)
+    if auth_code
+      # todo: authorization code flow
+    else
+      # todo: password flow
+    end
+  end
+
+  def refresh
+    return if @token == nil
+    # todo: refresh token
+  end
+
+  def revoke
+    return if @token == nil
+    # todo: revoke token
+  end
+
+  def authorize_uri(redirect_uri, state = '')
+    # todo: construct authorize_uri
+  end
 end
--- test/test_ringcentral.rb
+++ test/test_ringcentral.rb
@@ -2,8 +2,16 @@ require 'test/unit'
 require 'ringcentral'
 
 class RingCentralTest < Test::Unit::TestCase
-  def test_hello
+  def test_class_variables
     assert_equal 'https://platform.devtest.ringcentral.com', RingCentral.SANDBOX_SERVER
     assert_equal 'https://platform.ringcentral.com', RingCentral.PRODUCTION_SERVER
+  end
+
+  def test_initializer
+    rc = RingCentral.new('app_key', 'app_secret', RingCentral.SANDBOX_SERVER)
+    assert_equal 'app_key', rc.app_key
+    assert_equal 'app_secret', rc.app_secret
+    assert_equal 'https://platform.devtest.ringcentral.com', rc.server
+    assert_equal true, rc.auto_refresh
   end
 end
