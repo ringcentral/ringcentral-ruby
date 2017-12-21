8378f31de3da5cfafcb4b389692a5612e1abcce0
Tyler Long <tyler4long@gmail.com>
2017-12-21 16:12:02 +0800

    Update unit test

--- test/test_subscription.rb
+++ test/test_subscription.rb
@@ -3,16 +3,22 @@ require 'ringcentral'
 require 'dotenv'
 
 class SubscriptionTest < Minitest::Test
-  def test_create_subscription
+  def setup
     Dotenv.load
-    rc = RingCentral.new(ENV['appKey'], ENV['appSecret'], ENV['server'])
-    rc.authorize(username: ENV['username'], extension: ENV['extension'], password: ENV['password'])
+    @rc = RingCentral.new(ENV['appKey'], ENV['appSecret'], ENV['server'])
+    @rc.authorize(username: ENV['username'], extension: ENV['extension'], password: ENV['password'])
+  end
+
+  def test_create_subscription
     events = [
       '/restapi/v1.0/account/~/extension/~/message-store',
     ]
-    subscription = rc.subscription(events, lambda { |message|
-      puts message
+    count = 0
+    subscription = @rc.subscription(events, lambda { |message|
+      count += 1
     })
     subscription.subscribe()
+    sleep(15)
+    assert_operator count, :>, 0
   end
 end
